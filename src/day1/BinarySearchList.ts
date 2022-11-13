3d1481a860a0d7f72169a0027d5106192ba828e8
Ron Jean-Francois <rjfrancoisjr@icloud.com>
2022-11-13 11:55:38 -0500

    completed algo and passed tests

---
+++ src/day1/BinarySearchList.ts
@@ -0,0 +1,17 @@
+export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let hi = haystack.length;
+
+    do {
+        const mid = Math.floor(lo + (hi - lo) / 2);
+        const value = haystack[mid];
+        if (value === needle) {
+            return true;
+        } else if (value > needle) {
+            hi = mid;
+        } else {
+            lo = mid + 1;
+        }
+    } while (lo < hi);
+    return false;
+}
